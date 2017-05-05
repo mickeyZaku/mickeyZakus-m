1d4c6e318f5dfc1bfe0fcfe94e0b59b328099031
mickeyZaku <406817272@qq.com>
2017-05-05 17:19:02 +0800

    change to manager

--- index.js
+++ index.js
@@ -1,63 +1,213 @@
-var vm = new Vue({
-    el: '#app',
-    data: {
-        todos: [{title: '喂o(=•ェ•=)m', isChecked: true}, {title: '浇花', isChecked: false}, {
-            title: '学习',
-            isChecked: false
-        }],
-        temTodo: '',
-        state: '',
-        curTodo:'',
-        saveDate:''
+/**
+ * Created by 40681 on 2017/5/5.
+ */
+//轮播图组件
+let slider = {
+    template: '#slider',
+    props: {
+        datas: {type: Array, required: true},
+        delay: {type: Number}
+    },
+    data(){
+        return {index: 0, timer: null, show: true}
+    },
+    computed: {
+        prev(){
+            if (this.index === 0) {
+                return this.datas.length - 1;
+            } else {
+                return this.index - 1;
+            }
+        },
+        next(){
+            if (this.index === this.datas.length - 1) {
+                return 0;
+            } else {
+                return this.index + 1;
+            }
+        }
+    },
+    mounted(){
+        this.autoplay();
     },
     methods: {
-        remove(index){
-            this.todos.splice(index, 1);
+        go(index){
+            this.show = true;
+            setTimeout(() => {
+                this.index = index;
+                this.show = false;
+            }, 100);
+            // this.index = index;
         },
+        clear(){
+            clearInterval(this.timer);
+        },
+        autoplay(){
+            this.timer = setInterval(() => {
+                this.go(this.next);
+            }, this.delay);
+        }
+    }
+};
+//首页
+let home = {
+    template: '#home',
+    components: {
+        slider
+    },
+    data(){
+        return {
+            images: [{path: 'img/1.jpg', title: '1.jpg'}, {path: 'img/2.jpg', title: '2.jpg'}, {
+                path: 'img/3.jpg',
+                title: '3.jpg'
+            }]
+        }
+    },
+};
+//个人信息
+let profile = {
+    template: '#profile',
+};
+let info = {
+    template: '<div>o(=•ェ•=)m</div>'
+};
+let about = {
+    template: '<div>喵~~</div>'
+};
+//登录
+let login = {
+    template: '#login',
+    data(){
+        return {username:'',pwd:''};
+    },
+    methods:{
+      login(){
+            localStorage.setItem('login','true');
+            this.$emit('check');
+            this.$router.push('/home');
+      }
+    },
+    computed:{
+        userinfo(){
+            return this.username.length>3&&this.username.length<6;
+        }
+    }
+};
+//todoList
+let toDoList = {
+    template: '#toDoList',
+    computed: {
+        count(){
+            return this.todos.filter((item) => !item.isChecked).length;
+        }
+    },
+    methods: {
         addTodo(){
             this.todos.push({title: this.temTodo, isChecked: false});
             this.temTodo = '';
         },
-        changTitle(item){
+    },
+    data(){
+        return {
+            todos: [
+                {
+                    title: '喂o(=•ェ•=)m',
+                    isChecked: true
+                },
+                {
+                    title: '浇花',
+                    isChecked: false
+                }
+            ],
+            temTodo: '',
+        }
+    },
+    mounted(){
+        this.todos = JSON.parse(window.localStorage.getItem('todos')) || [];
+    }
+};
+let all = {
+        template: '#all',
+        props: {
+            todos: {required: true}
+        },
+        data(){
+            return {
+                curTodo: '',
+                origin: ''
+            }
+        },
+        directives: {
+            autoFocus(ele, bindings){
+                if (bindings.value) {
+                    ele.focus();
+                }
+            }
+        },
+        methods: {
+            removeTodo(index){
+                this.todos.splice(index, 1);
+            },
+            updateTitle(item){
+                this.curTodo = item;
+            },
+            reset(){
+                this.curTodo = '';
+            },
+            originDate(item){
+                this.origin = item.title;
+            },
+            toOrigin(item){
+                item.title = this.origin;
+            }
+        },
+        watch: {
+            todos: {
+                handler(){
+                    window.localStorage.setItem('todos', JSON.stringify(this.todos));
+                },
+                deep: true
+            }
+        },
+    };
+let unfinished = {
+    template: '#unfinished',
+    props: {
+        todos: {required: true}
+    },
+    computed:{
+        unTodos(){
+            return this.todos.filter((item)=>!item.isChecked);
+        }
+    },
+    data(){
+        return {
+            curTodo: '',
+            origin: ''
+        }
+    },
+    directives: {
+        autoFocus(ele, bindings){
+            if (bindings.value) {
+                ele.focus();
+            }
+        }
+    },
+    methods: {
+        removeTodo(index){
+            this.todos.splice(index, 1);
+        },
+        updateTitle(item){
             this.curTodo = item;
         },
         reset(){
-            this.curTodo='';
+            this.curTodo = '';
         },
-        originData(item){
-            this.saveDate = item.title;
+        originDate(item){
+            this.origin = item.title;
         },
         toOrigin(item){
-            item.title = this.saveDate;
-        }
-    },
-    directives:{
-      autoFocus(ele,bindings){
-          if (bindings.value) {
-              ele.focus();
-          }
-      }
-    },
-    computed: {
-        count(){
-            return this.todos.filter(function (item) {
-                return !item.isChecked;
-            }).length;
-        },
-        cloneTodos(){
-            if (this.state === 'all') {
-                return this.todos;
-            }
-            if (this.state === 'finished') {
-                return this.todos.filter(function (item) {
-                    return item.isChecked;
-                });
-            }
-            if(this.state==='unfinished'){
-                return this.todos.filter(function (item) {
-                    return !item.isChecked;
-                });
-            }
+            item.title = this.origin;
         }
     },
     watch: {
@@ -66,16 +216,100 @@ var vm = new Vue({
                 window.localStorage.setItem('todos', JSON.stringify(this.todos));
             },
             deep: true
-        },
-
+        }
     },
-    mounted(){
-        this.todos = JSON.parse(window.localStorage.getItem('todos')) || [];
+};
+let finished = {
+    template: '#unfinished',
+    props: {
+        todos: {required: true}
+    },
+    computed:{
+        unTodos(){
+            return this.todos.filter((item)=>item.isChecked);
+        }
+    },
+    data(){
+        return {
+            curTodo: '',
+            origin: ''
+        }
+    },
+    directives: {
+        autoFocus(ele, bindings){
+            if (bindings.value) {
+                ele.focus();
+            }
+        }
+    },
+    methods: {
+        removeTodo(index){
+            this.todos.splice(index, 1);
+        },
+        updateTitle(item){
+            this.curTodo = item;
+        },
+        reset(){
+            this.curTodo = '';
+        },
+        originDate(item){
+            this.origin = item.title;
+        },
+        toOrigin(item){
+            item.title = this.origin;
+        }
+    },
+    watch: {
+        todos: {
+            handler(){
+                window.localStorage.setItem('todos', JSON.stringify(this.todos));
+            },
+            deep: true
+        }
+    },
+};
+/**
+ *
+ *路由列表
+ */
+let routes = [
+    {path: '/home', component: home},
+    {path: '/', component: home},
+    {
+        path: '/profile',
+        component: profile,
+        children: [
+            {path: '/', component: info},
+            {path: 'info', component: info},
+            {path: 'about', component: about}
+        ]
+    },
+    {path: '/login', component: login},
+    {path: '/todoList', component: toDoList, children: [{path: 'all', component: all}, {path: 'unfinished', component: unfinished}, {path: 'finished', component: finished}, {path: '/', component: all}]},
+    {path: '/*', component: home},
+];
+/**
+ * 路由
+ * @type {VueRouter}
+ */
+let router = new VueRouter({
+    routes
+});
+/**
+ * 根组件
+ */
+let vue = new Vue({
+    el: '#app',
+    router,
+    data:{login:false},
+    methods:{
+        checkLogin(){
+            this.login = localStorage.getItem('login')||false;
+        },
+        logout(){
+            this.login = false;
+            this.$router.push('/home');
+            localStorage.removeItem('login');
+        }
     }
 });
-let listener = function () {
-    vm.state = location.hash.slice(1) || 'all';
-    console.log(vm.state);
-};
-listener();
-window.addEventListener('hashchange', listener);
